8585c960155cd1ba61f63a7c3e3f69f624ba8270
Dave Cross <dave@dave.org.uk>
2002-06-15 13:51:55 +0000

    Initial revision

---
+++ tt-mode.el
@@ -0,0 +1,58 @@
+;; tt-mode.el --- Emacs major mode for editing Template Toolkit files
+;;
+;; Copyright (c) 2002 Dave Cross, all rights reserved.
+;;
+;; This file may be distributed under the same terms as GNU Emacs.
+;;
+;; $Id$
+;;
+;; This file adds simple font highlighting of TT directives when you are 
+;; editing Template Toolkit files.
+;;
+;; I usually give these files an extension of .tt and in order to automatically
+;; invoke this mode for these files, I have the following in my .emacs file.
+;;
+;; (setq load-path
+;;      (cons "/home/dave/xemacs" load-path))
+;; (autoload 'tt-mode "tt-mode")
+;;(setq auto-mode-alist
+;;  (append '(("\\.tt$" . tt-mode))  auto-mode-alist ))
+;;
+;; Something similar may well work for you.
+;;
+;; $Log$
+;; Revision 1.1  2002/06/15 13:51:56  dave
+;; Initial revision
+;;
+;;
+
+(require 'font-lock)
+
+(defvar tt-mode-hook nil
+  "List of functions to call when entering TT mode")
+
+(defvar tt-font-lock-keywords 
+   (list
+    ;; Fontify [& ... &] expressions
+    '("\\(\\[%.*%\\]\\)"  1 font-lock-keyword-face t)
+    )
+  "Expressions to font-lock in tt-mode.")
+
+(defun tt-mode ()
+  "Major mode for editing Template Toolkit files"
+  (interactive)
+  (kill-all-local-variables)
+  (setq major-mode 'tt-mode)
+  (setq mode-name "TT")
+  (if (string-match "Xemacs" emacs-version)
+      (progn
+	(make-local-variable 'font-lock-keywords)
+	(setq font-lock-keywords tt-font-lock-keywords))
+    ;; Emacs
+    (make-local-variable 'font-lock-defaults)
+    (setq font-lock-defaults '(tt-font-lock-keywords nil t))
+    )
+  (font-lock-mode)
+  (run-hooks tt-mode-hook))
+
+(provide 'tt-mode)
